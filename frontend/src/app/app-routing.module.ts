beea895a5492eda894a3891fbb252876c3b649d1
Alex Huang <paojen.huang@gmail.com>
2021-07-05 11:34:27 -0400

    Remove New Point page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NewPointComponent } from './pages/new-point/new-point.component';
 import { PointViewComponent } from './pages/point-view/point-view.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'points', pathMatch: 'full' },
-  {path: 'points', component: PointViewComponent},
-  {path: 'points/:pointId', component: PointViewComponent},
-  {path: 'new-point', component: NewPointComponent}
+  { path: 'points', component: PointViewComponent },
+  { path: 'points/:pointId', component: PointViewComponent }
 ];
 
 @NgModule({

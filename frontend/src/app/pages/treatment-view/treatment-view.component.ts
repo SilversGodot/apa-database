d91dbdfb00ed53978d84635882b2f7da74aa3bab
Neal Huang <huangtzuchao@gmail.com>
2021-07-18 14:18:02 -0400

    Update to treatment backend

--- frontend/src/app/pages/treatment-view/treatment-view.component.ts
+++ frontend/src/app/pages/treatment-view/treatment-view.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, OnInit, ViewChild } from '@angular/core';
 import { animate, state, style, transition, trigger } from '@angular/animations';
 import { MatTable } from '@angular/material/table';
+import { MatDialog } from '@angular/material/dialog';
 
 import Treatment from 'src/app/models/treatment';
 import { TreatmentService } from '@app/services/treatment.service';
@@ -8,14 +9,24 @@ import { TreatmentService } from '@app/services/treatment.service';
 @Component({
   selector: 'app-treatment-view',
   templateUrl: './treatment-view.component.html',
-  styleUrls: ['./treatment-view.component.css']
+  styleUrls: ['./treatment-view.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ]
 })
 export class TreatmentViewComponent implements OnInit {
+  columnsToDisplay = ['name', 'points'];
+  expandedTreatment: Treatment | null;
   treatments: Treatment[] = [];
 
   @ViewChild(MatTable, { static: true }) table: MatTable<any>;
 
   constructor(
+    public dialog: MatDialog,
     private treatmentService: TreatmentService
   ) { 
   }

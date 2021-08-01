771bb4e0cd4a073e76b0fdc16bff51de83eddc15
Alex Huang <paojen.huang@gmail.com>
2021-08-01 13:42:15 -0400

    Reset backend url

--- frontend/src/app/web.service.ts
+++ frontend/src/app/web.service.ts
@@ -7,7 +7,7 @@ import { HttpClient } from '@angular/common/http';
 export class WebService {
   readonly ROOT_URL: string;
   constructor(private http : HttpClient) {
-    this.ROOT_URL = 'http://localhost:3000';
+    this.ROOT_URL = 'http://ec2-13-58-91-242.us-east-2.compute.amazonaws.com/api';
     // 'http://ec2-13-58-91-242.us-east-2.compute.amazonaws.com/api'
     // 'http://localhost:3000'
   }
